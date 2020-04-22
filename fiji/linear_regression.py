44202453bb42b13b21b6733de838a2cc93a0cd3f
Entangle 2 Giraffe <62873319+entangle2giraffe@users.noreply.github.com>
2020-04-23 02:59:48 +0700

    Add files via upload

--- fiji/linear_regression.py
+++ fiji/linear_regression.py
@@ -17,27 +17,33 @@ regr = linear_model.LinearRegression()
 
 # Train dataset
 # Convert List -> Array
-train_x = np.asanyarray(train("Year"))
-train_y = np.asanyarray(train("Value"))
+train_x = np.asanyarray(train[["Year"]])
+train_y = np.asanyarray(train[["Value"]])
 
 # Learning
 regr.fit(train_x, train_y)
 print(f"Coefficient: {float(regr.coef_)}")
 print(f"Intercept: {float(regr.intercept_)}")
 
-# Plot
-plt.scatter(train.Year, train.Value, color="green")
-plt.plot(train_x, regr.coef_[0][0] * train_x + regr.intercept_[0], color="red")
-plt.x_label("Year")
-plt.y_label("Government Consumption Expense (10^6 USD)")
-plt.title("Fiji Government Consumption Expense 1950 - 2005")
-plt.show()
-
 # Test dataset
-test_x = np.asanyarray(test("Year"))
-test_y = np.asanyarray(test["Value"])
+test_x = np.asanyarray(test[["Year"]])
+test_y = np.asanyarray(test[["Value"]])
 test_y_ = regr.predict(test_x)
 
 # Accuracy of this model
-print("Mean Squared Error: %.2f" % mean_squared_error(test_y_ - test_y))
-print("R2 Score: %.2f" % r2_score(test_y_ - test_y))
+mse = mean_squared_error(test_y_, test_y)
+r2 = r2_score(test_y_, test_y)
+print("Mean Squared Error: %.2f" % mse)
+print("R2 Score: %.2f" % r2)
+
+# Graph
+plt.scatter(train.Year, train.Value, label="train", color="indigo")
+plt.plot(train_x, regr.coef_[0][0] * train_x + regr.intercept_[0], label="prediction", color="red")
+plt.scatter(test.Year, test.Value, label="test", color="blue")
+plt.xlabel("Year")
+plt.ylabel("Government Consumption Expense (10^6 USD)")
+plt.title("Fiji Government Consumption Expense 1950 - 2005")
+plt.legend()
+
+# Plot
+plt.show()
